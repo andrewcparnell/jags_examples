49a5f473481dceb92470b3b476c4913c52888247
Andrew Parnell <andrew.parnell@mu.ie>
2021-02-03 11:44:04 +0000

    Update jags_spline2.R

--- R Code/jags_spline2.R
+++ R Code/jags_spline2.R
@@ -127,21 +127,24 @@ delta_post <- model_run$BUGSoutput$sims.list$delta
 sigma_post <- model_run$BUGSoutput$sims.list$sigma
 
 # object to store prediction interval
-PI <- matrix(nrow = length(alpha_post),
-               ncol = N)
-for(i in 1:nrow(pred)) {
-  PI[i,] <- rnorm(N,
-                  mean = alpha_post[i] + Z%*%delta_post[i,],
-                  sd = sigma_post[i])
+PI <- matrix(
+  nrow = length(alpha_post),
+  ncol = N
+)
+for (i in 1:nrow(pred)) {
+  PI[i, ] <- rnorm(N,
+    mean = alpha_post[i] + Z %*% delta_post[i, ],
+    sd = sigma_post[i]
+  )
 }
-PI_quantile <- apply(PI, 2, quantile, prob = c(0.25,0.5,0.75))
+PI_quantile <- apply(PI, 2, quantile, prob = c(0.25, 0.5, 0.75))
 
 # Plot the output with uncertainty bands
 plot(x, y)
 lines(x, alpha + Z %*% delta, col = "red") # True line
-lines(x, PI_quantile[2,], col = "blue") # Predicted line
-lines(x, PI_quantile[1,], col = "blue", lty = 2) # Predicted low
-lines(x, PI_quantile[3,], col = "blue", lty = 2) # Predicted high
+lines(x, PI_quantile[2, ], col = "blue") # Predicted line
+lines(x, PI_quantile[1, ], col = "blue", lty = 2) # Predicted low
+lines(x, PI_quantile[3, ], col = "blue", lty = 2) # Predicted high
 legend("topleft", c(
   "True line",
   "Posterior lines (with 50% CI)",
