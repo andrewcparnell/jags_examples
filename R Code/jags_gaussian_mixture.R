ed6007e99243e1020cb68b402aaf566658c9663f
Andrew Parnell <andrew.parnell@mu.ie>
2022-02-09 09:48:47 +0000

    Update jags_gaussian_mixture.R

--- R Code/jags_gaussian_mixture.R
+++ R Code/jags_gaussian_mixture.R
@@ -1,37 +1,28 @@
 # Header ------------------------------------------------------------------
 
-# Fitting a gaussian mixture model in JAGS
-# Bruna Wundervald
+# Fitting a Gaussian mixture model in JAGS
+# Bruna Wundervald & Andrew Parnell
 
-# In this file we fit a Bayesian Gaussian Mixture
+# In this file we fit a Bayesian Gaussian Mixture model using two simple components
 
 # Loading in required packages
 library(R2jags)
 library(tidyverse)
 
-# Description of the Bayesian Poisson model fitted in this file
+# Description of the Bayesian mixture model fitted in this file
 # Notation:
-# y_i = a mixture of gaussian distributions
-# mu_i = the location of the ith distribution/cluster
-# lambda_i = the weight of the ith gaussian distribution
-# sigma = the dispersion parameter for each Gaussian distribution
-# in the mixture
+# y_i = data point for observation i
+# mu_k = Location parameter (mean) of the kth cluster
+# lambda_k = the weight of the kth cluster (approximately the proportion of data points in the cluster)
+# sigma = the scale (standaard deviation) parameter for each Gaussian distribution in the mixture
 
-# Likelihood
-# y_i ~ GM = lambda_1 * G(mu_1, sigma) + lambda_2 * G(mu_2, sigma)
+# Likelihood for two components
+# y_i ~ GM = lambda_1 * N(mu_1, sigma) + lambda_2 * N(mu_2, sigma)
 
 # Priors - all vague
-# mu_1 ~ G(0, 10)
-# mu_2 ~ G(5, 10)
+# mu_k ~ N(0, 10^-2)
 # sigma ~ InverseGamma(0.01 ,0.01)
-# lambda ~ Dirichlet(1, 1)
-
-# Posteriors - all vague
-# mu_1 ~ G(0, 10)
-# mu_2 ~ G(5, 10)
-# sigma ~ InverseGamma(0.01 ,0.01)
-# lambda ~ Dirichlet(1, 1)
-
+# lambda_k ~ Dirichlet(1, 1, ...)
 
 # Simulate data -----------------------------------------------------------
 
@@ -44,59 +35,42 @@ mu_2 <- rnorm(n = 1, mean = 5, sd = 1)
 # True values for lambda_1 = lambda_2 = 0.5
 y <- c(rnorm(n = T, mean = mu_1, sd = 1), rnorm(n = T, mean = mu_2, sd = 1))
 
-Nclust <- 2
-N <- 2 * T
-clust <- rep(NA, N)
-clust[which.min(y)] <- 1 # smallest value assigned to cluster 1
-clust[which.max(y)] <- 2 # highest value assigned to cluster 2
-ones <- rep(1, Nclust)
-
-
-df <- y %>%
-  as.data.frame()
-names(df)[1] <- "y"
-
 # Have a quick look at the density of y
-df %>%
-  ggplot(aes(y)) +
-  geom_density(colour = "orange", size = 1.3) +
-  xlim(min(y) - 2, max(y) + 2) +
-  theme_bw()
+qplot(y, geom = 'histogram')
 
+
+# JAGS code ---------------------------------------------------------------
 
 model_code <- "
 model {
   # Likelihood:
   for(i in 1:N) {
-    y[i] ~ dnorm(mu[i] , 1/sigma_inv) 
+    y[i] ~ dnorm(mu[i] , sigma^-2) 
     mu[i] <- mu_clust[clust[i]]
-    clust[i] ~ dcat(lambda_clust[1:Nclust])
+    clust[i] ~ dcat(lambda[1:K])
   }
-  # Prior:
-  sigma_inv ~ dgamma( 0.01 ,0.01)
-  mu_clust[1] ~ dnorm(0, 10)
-  mu_clust[2] ~ dnorm(5, 10)
-  
-  lambda_clust[1:Nclust] ~ ddirch(ones)
+  # Priors
+  sigma ~ dgamma( 0.01 ,0.01)
+  for (k in 1:K) {
+    mu_clust_raw[k] ~ dnorm(0, 10^-2)
+  }
+  mu_clust <- sort(mu_clust_raw) # Ensure ordering to prevent label switching
+  lambda[1:K] ~ ddirch(ones)
 }
 "
 
 
 # Set up the data
-model_data <- list(Nclust = Nclust, y = y, N = N, ones = ones, clust = clust)
+model_data <- list(K = 2, y = y, N = length(y), ones = rep(1, K))
 
 # Choose the parameters to watch
-model_parameters <- c("sigma_inv", "mu_clust", "lambda_clust")
+model_parameters <- c("sigma", "mu_clust", "lambda")
 
 # Run the model
 model_run <- jags(
   data = model_data,
   parameters.to.save = model_parameters,
   model.file = textConnection(model_code),
-  n.chains = 4,
-  n.iter = 1000,
-  n.burnin = 200,
-  n.thin = 2
 )
 
 
@@ -109,32 +83,33 @@ print(model_run)
 traceplot(model_run)
 
 # Create a plot of the posterior desnsity line
-post <- print(model_run)
-sigma <- 1 / post$mean$sigma_inv
+post <- model_run$BUGSoutput
+sigma <- post$mean$sigma
 mu_clust <- post$mean$mu_clust
-lambda_clust <- post$mean$lambda_clust
+lambda <- post$mean$lambda
 
-N1 <- round(N * lambda_clust[1])
-N2 <- round(N * lambda_clust[2])
+N <- length(y)
+N1 <- round(N * lambda[1])
+N2 <- round(N * lambda[2])
+df <- data.frame(y = y)
 
 df <- df %>%
-  as.data.frame() %>%
-  mutate(mixt = c(
+ mutate(mixt = c(
     rnorm(n = N1, mean = mu_clust[1], sd = sqrt(sigma)),
     rnorm(n = N2, mean = mu_clust[2], sd = sqrt(sigma))
-  ))
-
+  )) %>% as.data.frame()
+  
 df %>%
   ggplot(aes(y)) +
   geom_density(colour = "orange", size = 1) +
   geom_density(
     data = df %>% slice(1:N1),
-    aes(mixt, y = ..density.. * lambda_clust[1]),
+    aes(mixt, y = ..density.. * lambda[1]),
     colour = "royalblue", linetype = "dotted", size = 1.1
   ) +
   geom_density(
     data = df %>% slice(N1 + 1:N2),
-    aes(mixt, y = ..density.. * lambda_clust[2]),
+    aes(mixt, y = ..density.. * lambda[2]),
     colour = "plum", linetype = "dotted", size = 1.1
   ) +
   xlim(min(y) - 2, max(y) + 2) +
@@ -165,40 +140,17 @@ clust[which.min(df$Petal.Width)] <- 1 # smallest value assigned to cluster 1
 clust[clust == quantile(df$Petal.Width, 1 / 3)] <- 2
 clust[which.max(df$Petal.Width)] <- 3 # highest value assigned to cluster 2
 
-model_code <- "
-model {
-  # Likelihood:
-  for(i in 1:N) {
-    y[i] ~ dnorm(mu[i] , 1/sigma_inv) 
-    mu[i] <- mu_clust[clust[i]]
-    clust[i] ~ dcat(lambda_clust[1:Nclust])
-  }
-  # Prior:
-  sigma_inv ~ dunif(1, 2)
-  mu_clust[1] ~ dnorm(0.2, 10)
-  mu_clust[2] ~ dnorm(1.6, 10)
-  mu_clust[3] ~ dnorm(2.2, 10)
-  
-  lambda_clust[1:Nclust] ~ ddirch(ones)
-}
-"
-
-
 # Set up the data
-model_data <- list(Nclust = 3, y = df$Sepal.Length, N = N, ones = c(1, 1, 1), clust = clust)
+model_data <- list(K = 3, y = df$Sepal.Length, N = N, ones = c(1, 1, 1), clust = clust)
 
 # Choose the parameters to watch
-model_parameters <- c("sigma_inv", "mu_clust", "lambda_clust")
+model_parameters <- c("sigma", "mu_clust", "lambda")
 
 # Run the model
 real_data_run <- jags(
   data = model_data,
   parameters.to.save = model_parameters,
-  model.file = textConnection(model_code),
-  n.chains = 4,
-  n.iter = 1000,
-  n.burnin = 200,
-  n.thin = 2
+  model.file = textConnection(model_code)
 )
 
 
@@ -210,37 +162,26 @@ traceplot(real_data_run)
 
 # Create a plot of the posterior desnsity line
 post <- print(real_data_run)
-sigma <- 1 / post$mean$sigma_inv
+sigma <- post$mean$sigma
 mu_clust <- post$mean$mu_clust
-lambda_clust <- post$mean$lambda_clust
+lambda_clust <- post$mean$lambda
 
 N1 <- round(N * lambda_clust[1])
 N2 <- round(N * lambda_clust[2])
 N3 <- round(N * lambda_clust[3])
 
 df <- df %>%
-  mutate(mixt = c(
-    rnorm(n = N1, mean = mu_clust[1], sd = sqrt(sigma)),
-    rnorm(n = N2, mean = mu_clust[2], sd = sqrt(sigma)),
-    rnorm(n = N3, mean = mu_clust[3], sd = sqrt(sigma))
-  ))
+  mutate(mixt1 = dnorm(df$Petal.Width, mean = mu_clust[1], sd = sqrt(sigma)),
+         mixt2 = dnorm(df$Petal.Width, mean = mu_clust[2], sd = sqrt(sigma)),
+         mixt3 = dnorm(df$Petal.Width, mean = mu_clust[3], sd = sqrt(sigma))
+  )
 
 df %>%
   ggplot(aes(Petal.Width)) +
   geom_density(colour = "orange", size = 1) +
-  geom_density(
-    data = df %>% slice(1:N1),
-    aes(mixt, y = ..density.. * lambda_clust[1]),
-    colour = "royalblue", linetype = "dotted", size = 1.1
-  ) +
-  geom_density(
-    data = df %>% slice(N1 + 1:N2),
-    aes(mixt, y = ..density.. * lambda_clust[2]),
-    colour = "plum", linetype = "dotted", size = 1.1
-  ) +
-  geom_density(
-    data = df %>% slice(N2 + 1:N3),
-    aes(mixt, y = ..density.. * lambda_clust[3]),
-    colour = "green", linetype = "dotted", size = 1.1
-  ) +
-  theme_bw()
+  geom_line(aes(x = Petal.Width, y = mixt1*lambda_clust[1]),
+               colour = "royalblue", linetype = "dotted", size = 1.1) + 
+  geom_line(aes(x = Petal.Width, y = mixt2*lambda_clust[2]),
+            colour = "plum", linetype = "dotted", size = 1.1) + 
+  geom_line(aes(x = Petal.Width, y = mixt3*lambda_clust[3]),
+            colour = "green", linetype = "dotted", size = 1.1)
